378c22d8fdb0c7f45260ba6221bcaa07e8b5de4b
David <david0406p2009@hotmail.com>
2021-04-16 23:44:02 -0600

    Componente de comentario terminado (NO FUNCIONAL)
    Datos quemados del display de comentarios

--- components/main/Post.js
+++ components/main/Post.js
@@ -1,102 +1,167 @@
 import React from 'react'
-import { Text, View, ImageBackground, StyleSheet, Image, Button, TouchableOpacity } from 'react-native'
-
+import { Text, View, StyleSheet, Image, Button, TouchableOpacity, SafeAreaView, StatusBar, FlatList } from 'react-native'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const scarlet = { uri: "https://m.media-amazon.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwNTUyNzc3Nw@@._V1_.jpg" }
 const galGadot = { uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Gal_Gadot_2018_cropped_retouched.jpg/1200px-Gal_Gadot_2018_cropped_retouched.jpg" }
-function Post() {
-    return (
-        <View style={styles.body}>
-            <View style={styles.container}>
-                <View style={styles.infoPersonal}>
-                    <View style={styles.containerImage}>
-                        <Image
-                            source={galGadot}
-                            style={styles.image}
-                        />
-                    </View>
-                    <View style={styles.containerInfo}>
-                        <View >
-                            <Text style={styles.userName}>Scarlett Johanson</Text>
-                            <Text style={{ fontSize: 11, color: "#9E9E9E", fontWeight: "bold" }}>Mar 5, 2021 - 8:40pm</Text>
-                        </View>
-                        <View style={{ marginRight: 10, justifyContent: "center" }}>
-                            <Text style={{ fontSize: 11, color: "#9E9E9E" }}>Semana 2</Text>
-                        </View>
-                    </View>
+const DATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'First Item',
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Second Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Third Item',
+    },
+];
+
+const Item = ({ title }) => (
+    <View style={styles.item}>
+        <View style={styles.infoConatiner}>
+            <View style={styles.postImage}>
+                <Image source={scarlet}
+                    style={styles.image} />
+            </View>
+            <View style={styles.infoPost}>
+                <View style={{ marginLeft: 10 }}>
+                    <Text style={{ fontSize: 18, color: '#1C1C1C', fontWeight: '500' }}>Gal Gadot</Text>
+                    <Text style={{ fontSize: 11, color: '#9E9E9E', marginLeft: 2 }}>Mar 5, 2021 - 8:40pm</Text>
                 </View>
-                <View style={styles.comentarioContainer}>
+                <View style={styles.week}>
+                    <Text style={{ fontSize: 11, color: '#9E9E9E' }}>semana 3</Text>
                 </View>
             </View>
-
+        </View>
+        <View style={styles.courseContainer}>
+            <View style={styles.courseImage}>
+                <MaterialCommunityIcons name="notebook" color="white" size={60} />
+            </View>
+            <View style={styles.courseInfoConainer}>
+                <Text style={{ fontSize: 24, fontWeight: "bold", color: "#1C1C1C" }}>Estructuras de datos</Text>
+                <View style={styles.courseInfo}>
+                    <Text style={{ fontSize: 16, color: "#373737", }}>Juan de dios</Text>
+                    <Text style={{ fontSize: 11, color: "#9E9E9E" }}>II ciclo</Text>
+                </View>
+            </View>
+        </View>
+        <View style={styles.comment}>
+            <Text style={{ fontSize: 12, color: '#4B4B4B', fontWeight: 'bold' }}>
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+            </Text>
         </View>
 
-    )
+    </View>
+);
+
+function Post() {
+    const renderItem = ({ item }) => (
+        <Item title={item.title} />
+    );
+
+    return (
+        <SafeAreaView style={{
+            flex: 1,
+            marginTop: StatusBar.currentHeight || 0
+        }}>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+            />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-    body: {
+    comment: {
+        flex: 3,
+        fontSize: 12,
+        paddingVertical: 8
+    },
+    courseImage: {
+        width: 80,
+        height: 80,
+        backgroundColor: "#F2CC39",
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 15
+    },
+    courseInfoConainer: {
         flex: 1,
-        backgroundColor: "#fff",
-        // justifyContent: "center",
-        flexDirection: "row"
+        justifyContent: 'space-between',
     },
-    container: {
+    courseInfo: {
         flex: 1,
-        flexDirection: "column",
-        margin: 10,
-        width: "100%",
-        // backgroundColor: "#000",
-        padding: 10,
-        borderWidth: 2,
-        borderColor: "#0004"
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'space-between',
     },
-    infoPersonal: {
-        alignItems: "center",
-        justifyContent: "space-between",
-        flexDirection: "row",
-        backgroundColor: "#fff",
-        borderBottomColor: "#D7D7D7",
-        borderBottomWidth: 1,
-        paddingTop: 10,
-        paddingBottom: 10,
-    },
-
-    containerImage: {
+    postImage: {
         alignItems: "center",
         justifyContent: "center",
         width: 50,
         height: 50,
         borderRadius: 25,
         overflow: "hidden",
-        backgroundColor: "#ff0000",
-        marginLeft: 5
     },
     image: {
-        width: 80,
-        height: 80,
+        width: 75,
+        height: 75,
         resizeMode: "contain",
         borderRadius: 40,
     },
-    containerInfo: {
+    week: {
+        fontSize: 12,
+    },
+    infoConatiner: {
         flex: 1,
-        marginLeft: 10,
         flexDirection: "row",
-        justifyContent: "space-between",
+        borderBottomWidth: 1,
+        borderBottomColor: "#EDECEC",
+        paddingVertical: 10,
+
     },
-    userName: {
-        fontSize: 20,
-        fontWeight: "600",
-        color: "#212121",
-    },
-    comentarioContainer: {
-        alignItems: "center",
-        justifyContent: "space-between",
+    infoPost: {
+        flex: 1,
         flexDirection: "row",
-        height: 100,
-        width: "100%",
+        justifyContent: 'space-between'
+    },
+    courseContainer: {
+        flex: 1,
+        flexDirection: "row",
+        marginVertical: 8,
+    },
+    item: {
+        marginVertical: 8,
+        height: "auto",
+        paddingHorizontal: 10,
         backgroundColor: "#fff",
+        borderColor: "#4d4d4d",
+        shadowColor: "#4d4d4d",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.8,
+        shadowRadius: 8.51,
+        elevation: 2,
     },
+    title: {
+        fontSize: 18,
+    },
+    body: {
+        flex: 1,
+        backgroundColor: "#fff",
+        // justifyContent: "center",
+        flexDirection: "row"
+    },
+
 
 
 });
